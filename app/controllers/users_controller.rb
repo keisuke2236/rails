ed2e4db8c001f50b02253465a18bed2fc21dec36
keisuke2236 <rorensu2236@gmail.com>
2015-08-05 15:11:43 +0900

    データベースのマイグレーション完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,17 @@ class UsersController < ApplicationController
 	end
 	def show
 		@user = Hash.new
-		@user[:name] = 'Shohei Aoki'
-		@user[:username] = 'moyahima'
-		@user[:location] = 'Tottori, Japan'
-		@user[:about] = 'Nice to meet you.'
+		if params[:username] == "mori"
+			@user[:name] = "mori"
+			@user[:username] = 'daikon'
+			@user[:location] = 'Tottori, Japan'
+			@user[:about] = 'Nice to meet you.'
+		elsif params[:username] == "yama"
+			@user[:name] = "yama"
+			@user[:username] = 'daikin'
+			@user[:location] = 'Tottori, Japan'
+			@user[:about] = 'Nice to meet you.'
+
+		end
 	end
 end
