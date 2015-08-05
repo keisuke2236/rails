ea3f40a22850ff6f7b1e83bbb2df071f4dc86ed7
keisuke2236 <rorensu2236@gmail.com>
2015-08-05 15:17:52 +0900

    データベースにデータを入れた

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+@user = User.new
+@user.name = 'mori'
+@user.username = 'yamada'
+@user.location = 'Tokyo'
+@user.about = "Heelo it is wao"
+@user.save
+@user = User.new
+@user.name = 'yama'
+@user.username = 'moyahima'
+@user.location = 'Tottori, Japan'
+@user.about = 'Nice to meet you. I am from database!'
+@user.save
