694b52f2880b17c31ed03e05dafb06dc954df58c
keisuke2236 <rorensu2236@gmail.com>
2015-08-15 20:40:59 +0900

    Twitter完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
+
+  get 'tweets/show'
+
+  get 'tweets/new'
+
   get 'users/index'
   get 'users/show'
   get 'users/show/:username' => "users#show"
+  post "tweets" => "tweets#create"
 end
